e767eb052096fa7915cc6c6e1a9dcd98029e0cfc
zachary4biz <zhoutong@apusapps.com>
2018-08-01 09:41:00 +0800

    分布式多机完成。

--- python/7-Tensorflow/Distributed/DeepFM_distributed_script.py
+++ python/7-Tensorflow/Distributed/DeepFM_distributed_script.py
@@ -27,7 +27,9 @@ flags.DEFINE_string('job_name', None, 'job name: worker or ps')
 flags.DEFINE_integer('task_index', None, 'Index of task within the job')
 # 选择异步并行，同步并行
 flags.DEFINE_integer("issync", None, "是否采用分布式的同步模式，1表示同步模式，0表示异步模式")
+# flags.DEFINE_integer('replace_for_str_nan','N/A',"替代字符特征中NaN的值")
 
+replace_for_str_nan = "N/A"
 def print_t(param):
     sys.stdout.flush()
     now = time.strftime("|%Y-%m-%d %H:%M:%S| ", time.localtime(time.time()))
@@ -65,7 +67,7 @@ def main(unused_argv):
     # deepFM网络的通用超参数
     dfm_params = {"use_fm": True, "use_deep": True, "embedding_size": 8, "dropout_fm": [1.0, 1.0],
                   "deep_layers": [32, 32], "dropout_deep": [0.5, 0.5, 0.5], "deep_layers_activation": tf.nn.relu,
-                  "epoch": 30, "batch_size": 1024, "learning_rate": 0.001, "optimizer_type": "adagrad",
+                  "epoch": 2, "batch_size": 10240, "learning_rate": 0.001, "optimizer_type": "adagrad",
                   "batch_norm": 1, "batch_norm_decay": 0.995, "l2_reg": 0.01, "verbose": True,
                   "eval_metric": roc_auc_score, "random_seed": 2017}
     print("进程号是: %s" % os.getpid())
@@ -75,6 +77,7 @@ def main(unused_argv):
 
     if FLAGS.job_name == 'ps':
         print_t("enter ps mode. \n")
+        DataManager.prepare_feature_dict(load_path="/data/houcunyue/zhoutong/data/CriteoData/test_data/train_sampled.txt",feature_dict_save_path="/data/houcunyue/zhoutong/data/CriteoData/test_data/feature_dict")
         server.join()
     elif FLAGS.job_name == 'worker':
         print_t("enter worker mode. \n")
@@ -83,9 +86,9 @@ def main(unused_argv):
         with tf.device(tf.train.replica_device_setter(worker_device="/job:worker/task:%d" % FLAGS.task_index,cluster=cluster)):
             # 加载数据用, 根据worker机的数量已经提前划分好各个worker使用的数据;
             # todo:后续需要改成动态提供数据,跑得越快的机器可以喂给它更多数据
-            path = "/data/houcunyue/zhoutong/data/CriteoData/test_data/train_splitted_0{task_index}.txt".format(task_index=FLAGS.task_index)
+            path = "/data/houcunyue/zhoutong/data/CriteoData/test_data/train_sampled_0{task_index}.txt".format(task_index=FLAGS.task_index)
             print_t("加载的文件路径是: %s" % path)
-            data_manager = DataManager(path=path.format(task_index=FLAGS.task_index),task_index=FLAGS.task_index,label_col='target')
+            data_manager = DataManager(path=path.format(task_index=FLAGS.task_index),task_index=FLAGS.task_index,label_col='target',feature_dict_save_path="/data/houcunyue/zhoutong/data/CriteoData/test_data/feature_dict")
             print_t("data_manager 构建完毕")
 
             #########################################################################
@@ -96,11 +99,12 @@ def main(unused_argv):
             # 创建纪录全局训练步数变量
             global_step = tf.Variable(0, name='global_step', trainable=False)
             # opt_adam = tf.train.AdamOptimizer(dfm.learning_rate, beta1=0.9, beta2=0.999, epsilon=1e-8)
-            opt_replica = tf.train.SyncReplicasOptimizer(dfm.opt,
-                                 replicas_to_aggregate=len(worker_spec),
-                                 total_num_replicas=len(worker_spec),
-                                 name="mnist_sync_replicas")
-            dfm.opt = opt_replica
+            if FLAGS.issync:
+                opt_replica = tf.train.SyncReplicasOptimizer(dfm.opt,
+                                     replicas_to_aggregate=len(worker_spec),
+                                     total_num_replicas=len(worker_spec),
+                                     name="mnist_sync_replicas")
+                dfm.opt = opt_replica
             dfm.optimizer = dfm.opt.minimize(dfm.loss,global_step = global_step)
             #############################################################################
             ######################    worker 配置训练方式及相应的初始化    #################
@@ -160,45 +164,85 @@ def main(unused_argv):
                     sv.start_queue_runners(dfm.sess,[chief_queue_runner])
                 ######################################################
                 ################## worker 执行训练迭代 #################
-                print_t("Training begins")
+                print_t("init_op begins")
                 dfm.sess.run(init_op)
+                print_t("Training begins")
                 dfm.fit(Xi_train=data_manager.Xi_train,Xv_train=data_manager.Xv_train,y_train=data_manager.y_train)
+                print_t("fit done")
+                print_t(str(dfm.train_result))
                 # 测试模型
                 Xi_valid, Xv_valid, y_valid = data_manager.Xi_valid, data_manager.Xv_valid, data_manager.y_valid
+                print_t("validation begin")
                 y_pred = dfm.predict(Xi_valid, Xv_valid)
 
                 print_t ('Worker {idx}: ')
                 result = dfm.evaluate(Xi_valid, Xv_valid, y_valid)
                 print_t("   result is %s" % str(result))
-                print_t(result)
-                print_t("   y_test is :")
-                print(y_valid)
-                print_t("   y_pred is :")
-                print(y_pred)
                 ######################################################
                 ################## 训练结束, 进行评测 #################
 
 
 class DataManager(object):
-    def __init__(self, path, task_index, label_col ='target'):
+    def __init__(self, path, feature_dict_save_path,task_index, label_col ='target'):
         self.label_col=label_col
         self.task_index = task_index
         # 分块读取文件,然后concat, 划分训练集、测试集
         print_t("   loading data ... ")
-        train_data,test_data,feature_dict = self.prepare(path)
-        self.Xi_train, self.Xv_train, self.y_train = self.parse(train_data,fd=feature_dict)
-        self.Xi_valid, self.Xv_valid, self.y_valid = self.parse(test_data,fd=feature_dict)
+        train_data,test_data,feature_dict = DataManager.prepare(path=path,feature_dict_path=feature_dict_save_path)
+        self.Xi_train, self.Xv_train, self.y_train = DataManager.parse(train_data, feature_dict=feature_dict)
+        self.Xi_valid, self.Xv_valid, self.y_valid = DataManager.parse(test_data, feature_dict=feature_dict)
         # 需要做类型转换
         self.ignor_cols = ['']
-        self.feature_size = feature_dict.feat_dim
+        self.feature_size = feature_dict['feature_dim']
         self.field_size = len(self.Xi_train[0])
+    # 准备好全部特征的 feature_dict
     @staticmethod
-    def prepare(path):
+    def prepare_feature_dict(load_path,feature_dict_save_path,chunk_size=100*10000):
         col_names = ['target'] + ["feature_%s" % i for i in range(39)]
-        # 已知前13列特征都是numeric
-        dtype_dict = {x:float for x in col_names[:13]}
-        for x in col_names[13:] : dtype_dict[x] = object
-        chunk_size = 200*10000
+        # 已知前13列特征都是numeric + 1列target
+        numeric_cnt = 14
+        dtype_dict = {x:float for x in col_names[:numeric_cnt]}
+        for x in col_names[numeric_cnt:] : dtype_dict[x] = object
+        numeric_cols=col_names[:numeric_cnt]
+        ignore_cols = ['target']
+        _reader = pd.read_csv(load_path, header=None,
+                              names=col_names,
+                              delimiter="\t",
+                              chunksize=chunk_size,
+                              dtype=dtype_dict)
+        na_dict = {}
+        for col in col_names[:numeric_cnt]:na_dict[col] = 0.0
+        for col in col_names[numeric_cnt:]:na_dict[col] = replace_for_str_nan
+        feature_dict = {}
+        tc = 0
+        i = 0
+        for df in _reader:
+            df.fillna(na_dict, inplace=True)
+            print_t("   处理第 %s 个chunk" % i)
+            i += 1
+            for col in df.columns:
+                if col in ignore_cols:
+                    continue
+                if col in numeric_cols:
+                    feature_dict[col] = tc
+                    tc += 1
+                else:
+                    us = df[col].unique()
+                    feature_dict[col] = dict(zip(us, range(tc, len(us)+tc)))
+                    tc += len(us)
+        feature_dict['feature_dim'] = tc
+        feature_dict['numeric_cols'] = numeric_cols
+        feature_dict['ignore_cols'] = ignore_cols
+        with open(feature_dict_save_path, 'w') as f:
+            f.write(str(feature_dict))
+    @staticmethod
+    def prepare(path,feature_dict_path):
+        col_names = ['target'] + ["feature_%s" % i for i in range(39)]
+        # 已知前13列特征都是numeric, 加上第一列 target 为0/1也是numeric
+        numeric_cnt = 14
+        dtype_dict = {x:float for x in col_names[:numeric_cnt]}
+        for x in col_names[numeric_cnt:] : dtype_dict[x] = object
+        chunk_size = 10*10000
         _reader = pd.read_csv(path, header=None,
                               names=col_names,
                               delimiter="\t",
@@ -206,33 +250,39 @@ class DataManager(object):
                               dtype=dtype_dict)
         train_data_chunks = []
         test_data_chunks = []
-        print_t("   loading data...")
+        print_t("----loading data...")
         for chunk in _reader:
             df_chunk = chunk
             cut_idx = int(0.8*df_chunk.shape[0])
             train_data_chunks.append(df_chunk[:cut_idx])
             test_data_chunks.append(df_chunk[cut_idx:])
-            print_t("   已拼接 %s 个 %s 行的chunk" % (len(train_data_chunks), chunk_size))
-        print_t("   concatting data...")
+            print_t("----已拼接 %s 个 %s 行的chunk" % (len(train_data_chunks), chunk_size))
+        print_t("----concatting data...")
         dfTrain = pd.concat(train_data_chunks, ignore_index=True)
         dfTest = pd.concat(test_data_chunks, ignore_index=True)
-        print_t("   feature_dict generating ...")
-        fd = FeatureDictionary(dfTrain=dfTrain, dfTest=dfTest,numeric_cols=list(dfTrain.select_dtypes(include=['float64', 'int64'], exclude=None).columns))
+        print_t("----feature_dict generating ...")
+        # fd = FeatureDictionary(dfTrain=dfTrain, dfTest=dfTest,numeric_cols=numeric_cols,ignore_cols=ignore_cols)
+        with open(feature_dict_path,'r') as f:
+            fd = eval(f.read())
+
+        print_t("----特征feature_size : %s" % str(fd['feature_dim']))
         return dfTrain,dfTest,fd
+
     @staticmethod
-    def parse(input_data,fd):
+    def parse(input_data, feature_dict):
         dfi = input_data.copy().drop(columns=['target'])
         dfv = dfi.copy()
         for col in dfi.columns:
-            if col in fd.ignore_cols:
+            if col in feature_dict['ignore_cols']:
                 dfi.drop(col, axis=1, inplace=True)
                 dfv.drop(col, axis=1, inplace=True)
                 continue
-            if col in fd.numeric_cols:
-                dfi[col] = fd.feat_dict[col]
+            if col in feature_dict['numeric_cols']:
+                dfi[col] = feature_dict[col]
                 dfv[col] = dfv[col].fillna(0.0)
             else:
-                dfi[col] = dfi[col].map(fd.feat_dict[col])
+                dfi[col].fillna(replace_for_str_nan, inplace=True)
+                dfi[col] = dfi[col].map(feature_dict[col])
                 dfv[col] = 1
         y = input_data['target'].values.tolist()
         Xi = dfi.values.tolist()
